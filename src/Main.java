4708026d0ba4955839df9e1dddeb113fc1bab69c
xsalles <xs.salles@gmail.com>
2024-11-04 16:23:01 -0300

    refatored: used try catch to show if something is wrong.

--- src/Main.java
+++ src/Main.java
@@ -15,39 +15,39 @@ public class Main {
         cpf = sc.next();
         cpf = cpf.replaceAll("[^0-9]", "");
 
-        if (cpf.length() != 11) {
-            System.out.println("CPF INVÁLIDO");
-        }
+        try {
+            peso = 10;
 
-        peso = 10;
+            for (int i = 0; i < 9; i++) {
+                sumFirstDigit += Character.getNumericValue(cpf.charAt(i)) * peso--;
+            }
+            firstDigit = (sumFirstDigit * 10) % 11;
 
-        for (int i = 0; i < 9; i++) {
-            sumFirstDigit += Character.getNumericValue(cpf.charAt(i)) * peso--;
-        }
+            firstDigit = (firstDigit == 10) ? 0 : firstDigit;
 
-        firstDigit = (sumFirstDigit * 10) % 11;
+            
+            peso = 11;
 
-        firstDigit = (firstDigit == 10) ? 0 : firstDigit;
+            for (int i = 0; i < 9; i++) {
 
+                sumSecondDigit += Character.getNumericValue(cpf.charAt(i)) * peso--;
+            }
 
-        peso = 11;
+            sumSecondDigit += firstDigit * peso;
 
-        for (int i = 0; i < 9; i++) {
+            secondDigit = (sumSecondDigit * 10) % 11;
 
-            sumSecondDigit += Character.getNumericValue(cpf.charAt(i)) * peso--;
-        }
-        
-        sumSecondDigit += firstDigit * peso;
+            secondDigit = (secondDigit == 10) ? 0 : secondDigit;
 
-        secondDigit = (sumSecondDigit * 10) % 11;
+            if (firstDigit != Character.getNumericValue(cpf.charAt(9))
+                    || secondDigit != Character.getNumericValue(cpf.charAt(10)) || cpf.length() != 11) {
+                System.out.println("CPF INVÁLIDO");
+            } else {
+                System.out.println("CPF VÁLIDO");
+            }
+        } catch (Exception e) {
+            System.out.println("Alguma coisa deu errado");
 
-        secondDigit = (secondDigit == 10) ? 0 : secondDigit;
-
-        if (firstDigit != Character.getNumericValue(cpf.charAt(9))
-                || secondDigit != Character.getNumericValue(cpf.charAt(10)) ) {
-            System.out.println("CPF INVÁLIDO");
-        } else {
-            System.out.println("CPF VÁLIDO");
         }
 
         sc.close();
